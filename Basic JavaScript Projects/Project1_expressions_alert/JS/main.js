b24cb93ffd2fe5ebd9c93d04967514c323278521
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-02 22:32:06 +0000

    added coments

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,3 @@
-var A = "Now i do \"Escape character\" " + "now this is second part " + "adding the plus sign\'s\ ";
+var A = "Now i do \"Escape character\" " + "now this is second part " + "adding the plus sign\'s\ "; // named var 
 
-document.write(A);
+document.write(A);  // caled my str
