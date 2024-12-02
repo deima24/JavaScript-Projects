29ee01ff182659ef16d2750d45fd170c99b80034
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-02 22:37:36 +0000

    Basic JavaScript Projects Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,7 @@
-var A = "Now i do \"Escape character\" " + "now this is second part " + "adding the plus sign\'s\ "; // named var 
+var Sent1 = "This is the beginning of the string"; // This is the STR 1
 
-document.write(A);  // caled my str
+window.alert(Sent1);                                // and mow im calling it in a alert
+
+var Sent2 = "and this is the end of the string"; // and this is second STR
+
+document.write(Sent2);                          // and now its calling the second STR
