f21e2a7e7243f1e4f2e6c86c1918d92f6560bb48
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-04 17:13:25 +0000

    Multiplication and Division Assignment

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,8 +1,32 @@
+//addition
 function addition_function(p1, p2) {
     return p1 + p2;
 }
 
 let x = addition_function(7, 8);
-document.getElementById("Math").innerHTML = x;
+document.getElementById("Math").innerHTML = "7 + 8 = " + x;
+
+//subtraction
+function subtraction_function(s1, s2) {
+    return s1 - s2;
+}
+
+let y = subtraction_function(10, 2);
+document.getElementById("subtract").innerHTML ="10 - 2 = " + y;
+
+// multiplication
+function multiplication_function(m1, m2) {
+    return m1 * m2;   
+}
+
+var multi_math = multiplication_function(5, 8);
+document.getElementById("multi").innerHTML = "5 x 8 = " + multi_math;
 
 
+//division
+function division_function(d1, d2) {
+    return d1 / d2;
+}
+
+var divi_math = division_function(50, 5);
+document.getElementById("divi").innerHTML = "50 / 5 = " + divi_math;
