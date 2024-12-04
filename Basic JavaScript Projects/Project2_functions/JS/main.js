43d90238522e99450b995a8beb8e823cb8a62533
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-04 11:12:23 +0000

    Function Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function First_One() {
+    var first_str = "My name is ";
+    var second_str = "Deima.";
+
+    document.getElementById("first_function").innerHTML = first_str;
+    document.getElementById("second_function").innerHTML = second_str;
+}
