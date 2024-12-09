adf556950dac4afe1b9265fa9db56a5daa1765ad
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-09 22:25:46 +0000

    started project 4

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function my_Colors(){
+    var colors = {
+        Tree: "Green",
+        Sun: "Yeallow",
+        Sky: "Blue",
+        Clouds: "White",
+        Clouds: "Dark",
+        Space: "Black"
+    };
+    document.getElementById("Dictionary").innerHTML = colors.Clouds;
+}
