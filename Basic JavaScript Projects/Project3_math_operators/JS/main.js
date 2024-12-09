751d89de5c25637112c9ff23c74626c9e152fd45
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-09 22:03:37 +0000

    added coment to js file about function

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,75 +1,71 @@
 //addition
-function addition_function(p1, p2) {
-    return p1 + p2;
+function addition_function() {      // naming function
+    var add = 7 + 8;
+    document.getElementById("Math").innerHTML = "7 + 8 = " + add;       // print result
 }
 
-let x = addition_function(7, 8);
-document.getElementById("Math").innerHTML = "7 + 8 = " + x;
 
 //subtraction
-function subtraction_function(s1, s2) {
-    return s1 - s2;
+function subtraction_function() {       //naming function
+    var y = 10 - 2;
+    document.getElementById("subtract").innerHTML ="10 - 2 = " + y;     // print result
 }
 
-let y = subtraction_function(10, 2);
-document.getElementById("subtract").innerHTML ="10 - 2 = " + y;
 
 // multiplication
-function multiplication_function(m1, m2) {
-    return m1 * m2;   
+function multiplication_function() {        //naming function
+    var multi_math = 5 * 8;
+    document.getElementById("multi").innerHTML = "5 x 8 = " + multi_math;   // printing result
 }
 
-var multi_math = multiplication_function(5, 8);
-document.getElementById("multi").innerHTML = "5 x 8 = " + multi_math;
 
 
 //division
-function division_function(d1, d2) {
-    return d1 / d2;
+function division_function() {      //naming function
+    var divi_math = 50 / 5;
+    document.getElementById("divi").innerHTML = "50 / 5 = " + divi_math;    // returning result
 }
 
-var divi_math = division_function(50, 5);
-document.getElementById("divi").innerHTML = "50 / 5 = " + divi_math;
 
 
 // Multiple Operations
-function Multiple_Operation() {
+function Multiple_Operation() {     // naming function
     var multiple_Math = (2 + 5) * 5 / 7 - 3;
-    document.getElementById("multiple").innerHTML = "2 plus 5, multiply by 5, divided by 7 and subtracted by 3 = " + multiple_Math;
+    document.getElementById("multiple").innerHTML = "(2 + 5) * 5 / 7 - 3 = " + multiple_Math;       // printing result
 }
 
 // moduls operator
-function Moduls_Operator() {
+function Moduls_Operator() {        // naming function
     var modul_Operator = 25 % 6;
-    document.getElementById("modul").innerHTML = "25 devided by 6 you get remainder: " + modul_Operator;
+    document.getElementById("modul").innerHTML = "25 devided by 6 you get remainder: " + modul_Operator;    // print result
 }
 
 
 // negation operator
-function Negation_Operator()  {
+function Negation_Operator()  {     // naming function
     var Negation_Operators = 6;
-    document.getElementById("negation").innerHTML = -Negation_Operators;
+    document.getElementById("negation").innerHTML = -Negation_Operators;        // returning result
 }
 
 
 var e = 6;
 
 //increment number
-function incrementNumber() {
+function incrementNumber() {        // naming function
     e++;
-    document.getElementById("counter").innerHTML = e;
+    document.getElementById("counter").innerHTML = e;       // returning incrementer result
 }
 
 
 //decrement number
-function decrementNumber() {
+function decrementNumber() {        // naming function
     e--;
-    document.getElementById("counter").innerHTML = e;
+    document.getElementById("counter").innerHTML = e;       // returning decremented result
 }
 
 // random number
-function random_Number(min, max) {
+function random_Number(min, max) {      // naming function
     var randomNum = Math.floor(Math.random() * (max - min + 1)) + min; // gets a whole number
     document.getElementById("math_random").innerHTML = randomNum;
-    return randomNum;
+    return randomNum;                                               // returning result
 }
