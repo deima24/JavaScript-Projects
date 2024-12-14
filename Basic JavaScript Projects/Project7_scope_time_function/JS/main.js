cd73157d68e205a19b33537729088f7d03a066a1
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-14 21:40:15 +0000

    Time function

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -4,7 +4,7 @@ function oneTry() {
 }
 
 function twotry() {
-    document.write(x + 20 + "<br>");        // global variable
+    document.write(x + 20);        // global variable
 }
 
 oneTry();
@@ -23,3 +23,42 @@ treeTry();
 fourtry();
 
 
+function get_Hours() {
+    var currentDate = new Date();
+    var hours = currentDate.getHours()
+    document.getElementById("dateHours").innerHTML = hours;
+}
+
+function get_Night() {
+    if (new Date().getHours() >= 21) {
+        document.getElementById("myTime").innerHTML = "Good night."
+    }
+    else 
+    document.getElementById("myTime").innerHTML = "Hello!"
+}
+
+function get_Age() {
+    age = document.getElementById("age").value;
+    if ( age >= 18) {
+        Drive = "You are old enough to drive.";
+    }
+    else {
+        Drive = "You are not old enaugh to drive.";
+    }
+    document.getElementById("your_Age").innerHTML = Drive;
+}
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
