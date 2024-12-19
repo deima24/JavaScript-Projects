446aa573bc228af12b857052d83ca47c3c088e6f
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-19 16:32:18 +0000

    Const Keyword Assignment

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -13,3 +13,33 @@ function length_function() {
     let sentence_length = text.length;                                       // gets length of string
     document.getElementById("demo").innerHTML = "The lenght is: " + sentence_length;         // display result
 }
+
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+
+function car_Array_Function() {
+    var cars = [" Audi ", " BMW ", " Ford ", " Volvo ", " Mercedez "];
+    document.getElementById("car_Array").innerHTML = "This is a car list: " + cars;
+}
+
+const myObject = {
+    name: "Tom",
+    age: "25",
+    city: "London"
+};
+
+function constant_function() {
+    myObject.age = 35;
+    myObject.country = "UK";
+    document.getElementById("Constant").innerHTML = "Name " + myObject.name + ". Age is " + myObject.age + " Lives in " + myObject.city + " Country is " + myObject.country;
+}
