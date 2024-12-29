4f1f1cfb2f9b64ca8b8d6c9750d397f46ce938a3
deima24 <111892514+deima24@users.noreply.github.com>
2024-12-29 18:46:02 +0000

    check win condition function added

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -41,7 +41,7 @@ function placeXOrO(squareNumber) {
             // this function disables clicking for computers turn
             disableClick();
             // this function waits 1sec before the computer places an image and enables click
-            setTimeout(function () { computersTurn();},  100);
+            setTimeout(function () { computersTurn(); },  1000);
         }
         // returning true is needed for our computersTurn() function to work
         return true;
@@ -64,5 +64,63 @@ function placeXOrO(squareNumber) {
                 success = true;
             };
         }
+    }
+}
+
+// this function parses the selectedSquares arrasy to search for win condition
+// drawLine() function is called to draw a line on the screen if the condition is met
+function checkWinConditions() {
+    // x 0, 1, 2 condition.
+    if (arrayIncludes('0x', '1x', '2x')) { drawWinLine(50, 100, 558, 100) }
+    //x 3, 4, 5 condition
+    else if (arrayIncludes('3x', '4x', '5x')) { drawWinLine(50, 304, 558, 304) }
+    // x 6, 7, 8 condition
+    else if (arrayIncludes('6x', '7x', '8x')) { drawWinLine(50, 508, 558, 508) }
+    // x 0, 3, 6 condition
+    else if (arrayIncludes('0x', '3x', '6x')) { drawWinLine(100, 50, 100, 558) }
+    // x 1, 4, 7 condition
+    else if (arrayIncludes('1x', '4x', '7x')) { drawWinLine(304, 50, 304, 558) }
+    // x 2, 5, 8 condition
+    else if (arrayIncludes('2x', '5x', '8x')) { drawWinLine(508, 50, 508, 558) }
+    // x 6, 4, 2 condition
+    else if (arrayIncludes('6x', '4x', '2x')) { drawWinLine(100, 508, 510, 90) }
+    // x 0, 4, 8 condition
+    else if (arrayIncludes('0x', '4x', '8x')) { drawWinLine(100, 100, 520, 520) }
+    // o 0, 1, 2 condition
+    else if (arrayIncludes('0o', '1o', '2o')) { drawWinLine(50, 100, 558, 100) }
+    // o 3, 4, 5 condition
+    else if (arrayIncludes('3o', '4o', '5o')) { drawWinLine(50, 304, 558, 304) }
+    // o 6, 7, 8 condition
+    else if (arrayIncludes('6o', '7o', '8o')) { drawWinLine(50, 508, 558, 508) }
+    // o 0, 3, 6 condition
+    else if (arrayIncludes('0o', '3o', '6o')) { drawWinLine(100, 50, 100, 558) }
+    // o 1, 4, 7 condition
+    else if (arrayIncludes('1o', '4o', '7o')) { drawWinLine(304, 50, 304, 558) }
+    // o 2, 5, 8 condition
+    else if (arrayIncludes('2o', '5o', '8o')) { drawWinLine(508, 50, 508, 558) }
+    // o 6, 4, 2 condition
+    else if (arrayIncludes('6o', '4o', '2o')) { drawWinLine(100, 508, 510, 90) }
+    // o 0, 4, 8 condition
+    else if (arrayIncludes('0o', '4o', '8o')) { drawWinLine(100, 100, 520, 520) }
+
+    // this condition checks for a tie. If none of the above conditions are met and 
+    // 9 squares are selected the code executes.
+    else if (selectedSquares.length >= 9) {
+        // this function plays the tie game sound
+        audio('./media/tie.mp3');
+        // this function sets a .3 second timer before the reset game is called
+        setTimeout(function () { resetGame(); }, 500);
+    }
+
+    // this function checks if an array includes 3 strings. It is used  to check for 
+    // each win condition
+    function arrayIncludes(squareA, squareB, squareC) {
+        // these 3 variables will be used to check for 3 in a row 
+        const a = selectedSquares.includes(squareA);
+        const b = selectedSquares.includes(squareB);
+        const c = selectedSquares.includes(squareC);
+        // if the 3 variables we pass are all included in our array then
+        // true is returned and our else if condition executes the drawLine() function
+        if (a=== true && b === true && c === true) { return true; }
     }
 }
